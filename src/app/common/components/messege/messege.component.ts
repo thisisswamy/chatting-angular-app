0c246fcbd4b83f54d5c7f462d75fdeb33879523b
Swamy <thudumswamy786@gmail.com>
2022-08-31 15:09:08 +0530

    add chat view

--- src/app/common/components/messege/messege.component.ts
+++ src/app/common/components/messege/messege.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessegeComponent implements OnInit {
 
+  userChatList:any[]=[
+    {
+      from:'Hello ram',
+      fromDate:'12/2/2022',
+      to:'Hello Bheem Hw ru',
+      toDate:'22/3/20'
+    },
+    {
+      from:'Em chestunav Mowa..',
+      fromDate:'12/2/2022',
+      to:'Em le mowa kali ye',
+      toDate:'22/3/20'
+    },
+    {
+      from:'Having funn ha',
+      fromDate:'12/2/2022',
+      to:'Nope....!',
+      toDate:'22/3/20'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
